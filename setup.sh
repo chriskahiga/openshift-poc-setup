481e97d916492da43e5f9859eb46b8095ad73e0e
Chris K.T <criskahiga@gmail.com>
2022-07-18 21:18:21 +0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -45,8 +45,8 @@ if [ -f "$CONFIG" ]; then
     IFS=. read -r m1 m2 m3 m4 <<<"$NETMASK"
     NET_ID="$((i1 & m1))"."$((i2 & m2))"."$((i3 & m3))"."$((i4 & m4))"
     #Calculate ip range
-    LOWER_LIMIT="$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$(((i4 & m4)+1))"
-    UPPER_LIMIT="$((i1 & m1 | 255-m1)).$((i2 & m2 | 255-m2)).$((i3 & m3 | 255-m3)).$(((i4 & m4 | 255-m4)-1))"
+    LOWER_LIMIT="$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$(((i4 & m4) + 1))"
+    UPPER_LIMIT="$((i1 & m1 | 255 - m1)).$((i2 & m2 | 255 - m2)).$((i3 & m3 | 255 - m3)).$(((i4 & m4 | 255 - m4) - 1))"
 
     #Generate variable file to be used by ansible playbooks
     cd ocp4_ansible/
@@ -55,113 +55,111 @@ $(<vars/template.yml)
 EOF
 " >vars/main.yml
 
-#Begin environment setup
-LOGFILE=$WORK_DIR/update.log
-touch $LOGFILE
-echo -e "\nInstalling DHCP ..\n"
-echo -e "Installing DHCP .." >> $LOGFILE
-sudo yum -y remove dhcp-server >> $LOGFILE
-sudo yum -y install dhcp-server >> $LOGFILE
-sudo systemctl enable dhcpd >> $LOGFILE
-sudo mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak >>$LOGFILE
-on_error $? "Issue installing dhcpd package. Check logs at $LOGFILE"
-echo -e "OK" >>$LOGFILE
+    #Begin environment setup
+    LOGFILE=$WORK_DIR/update.log
+    touch $LOGFILE
+    echo -e "\nInstalling DHCP ..\n"
+    echo -e "Installing DHCP .." >>$LOGFILE
+    sudo yum -y remove dhcp-server >>$LOGFILE
+    sudo yum -y install dhcp-server >>$LOGFILE
+    sudo systemctl enable dhcpd >>$LOGFILE
+    sudo mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak >>$LOGFILE
+    on_error $? "Issue installing dhcpd package. Check logs at $LOGFILE"
+    echo -e "OK" >>$LOGFILE
 
-echo -e "\nSetting up DHCP ..\n"
-echo -e "Setting up DHCP .." >> $LOGFILE
-ansible-playbook tasks/configure_dhcpd.yml >>$LOGFILE
-on_error $? "Issue setting up DHCP. Check logs at $LOGFILE"
-echo -e "OK" >>$LOGFILE
-echo -e "DHCP Setup Complete\n"
+    echo -e "\nSetting up DHCP ..\n"
+    echo -e "Setting up DHCP .." >>$LOGFILE
+    ansible-playbook tasks/configure_dhcpd.yml >>$LOGFILE
+    on_error $? "Issue setting up DHCP. Check logs at $LOGFILE"
+    echo -e "OK" >>$LOGFILE
+    echo -e "DHCP Setup Complete\n"
 
-echo -e "\nInstalling TFTP Server ..\n"
-echo -e "\nInstalling TFTP Server ..\n" >> $LOGFILE
-sudo yum remove -y tftp-server syslinux >> $LOGFILE
-sudo yum -y install tftp-server syslinux >> $LOGFILE
-sudo firewall-cmd --add-service=tftp --permanent
-sudo firewall-cmd --reload
-rm -f /etc/systemd/system/helper-tftp.service >> $LOGFILE
-cp files/helper-tftp.service /etc/systemd/system/helper-tftp.service >> $LOGFILE
-rm -f /usr/local/bin/start-tftp.sh >> $LOGFILE
-sudo tee /usr/local/bin/start-tftp.sh<<EOF
-#!/bin/bash
+    echo -e "\nInstalling TFTP Server ..\n"
+    echo -e "\nInstalling TFTP Server ..\n" >>$LOGFILE
+    sudo yum remove -y tftp-server syslinux >>$LOGFILE
+    sudo yum -y install tftp-server syslinux >>$LOGFILE
+    sudo firewall-cmd --add-service=tftp --permanent
+    sudo firewall-cmd --reload
+    rm -f /etc/systemd/system/helper-tftp.service >>$LOGFILE
+    cp files/helper-tftp.service /etc/systemd/system/helper-tftp.service >>$LOGFILE
+    rm -f /usr/local/bin/start-tftp.sh >>$LOGFILE
+    sudo echo '#!/bin/bash
 /usr/bin/systemctl start tftp > /dev/null 2>&1
 ##
-##
-EOF >> $LOGFILE
-sudo chmod a+x /usr/local/bin/start-tftp.sh
-sudo systemctl daemon-reload
-sudo systemctl enable --now tftp helper-tftp
-on_error $? "Issue installing TFTP. Check logs at $LOGFILE"
-echo -e "\nTFTP Installed\n"
-echo -e "OK" >>$LOGFILE
+##' >>/usr/local/bin/start-tftp.sh
+    sudo chmod a+x /usr/local/bin/start-tftp.sh
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now tftp helper-tftp
+    on_error $? "Issue installing TFTP. Check logs at $LOGFILE"
+    echo -e "\nTFTP Installed\n"
+    echo -e "OK" >>$LOGFILE
 
-sudo rm -rf /var/lib/tftpboot >> $LOGFILE
-sudo mkdir -p  /var/lib/tftpboot/pxelinux.cfg
-sudo cp -rvf /usr/share/syslinux/* /var/lib/tftpboot
-sudo mkdir -p /var/lib/tftpboot/rhcos
+    sudo rm -rf /var/lib/tftpboot >>$LOGFILE
+    sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg
+    sudo cp -rvf /usr/share/syslinux/* /var/lib/tftpboot
+    sudo mkdir -p /var/lib/tftpboot/rhcos
 
-echo -e "\nDownloading Required Files ..\n"
-echo -e "\nDownloading Required Files ..\n" >> $LOGFILE
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-installer-kernel-x86_64 >> $LOGFILE
-on_error $? "Could not download kernel file. Check logs at $LOGFILE"
-sudo mv rhcos-installer-kernel-x86_64 /var/lib/tftpboot/rhcos/kernel
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-installer-initramfs.x86_64.img >> $LOGFILE
-on_error $? "Could not download installer image. Check logs at $LOGFILE"
-sudo mv rhcos-installer-initramfs.x86_64.img /var/lib/tftpboot/rhcos/initramfs.img
-sudo restorecon -RFv  /var/lib/tftpboot/rhcos
-echo -e "\nFiles Successfully Downloaded"
-ls /var/lib/tftpboot/rhcos
-echo -e "OK" >> $LOGFILE
+    echo -e "\nDownloading Required Files ..\n"
+    echo -e "\nDownloading Required Files ..\n" >>$LOGFILE
+    wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-installer-kernel-x86_64 >>$LOGFILE
+    on_error $? "Could not download kernel file. Check logs at $LOGFILE"
+    sudo mv rhcos-installer-kernel-x86_64 /var/lib/tftpboot/rhcos/kernel
+    wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-installer-initramfs.x86_64.img >>$LOGFILE
+    on_error $? "Could not download installer image. Check logs at $LOGFILE"
+    sudo mv rhcos-installer-initramfs.x86_64.img /var/lib/tftpboot/rhcos/initramfs.img
+    sudo restorecon -RFv /var/lib/tftpboot/rhcos
+    echo -e "\nFiles Successfully Downloaded"
+    ls /var/lib/tftpboot/rhcos
+    echo -e "OK" >>$LOGFILE
 
-echo -e "Installing Apache ..\n"
-echo -e "Installing Apache ..\n" >> $LOGFILE
-sudo yum -y remove httpd >> $LOGFILE
-sudo yum -y install httpd >> $LOGFILE
-on_error $? "Could not install Apache. Check logs at $LOGFILE"
-sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
-systemctl enable --now httpd
-sudo firewall-cmd --add-port=8080/tcp --permanent
-sudo firewall-cmd --reload
-sudo mkdir -p /var/www/html/rhcos
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-live-rootfs.x86_64.img >> $LOGFILE
-on_error $? "Could not download Red Hat CoreOSrootfs image. Check logs at $LOGFILE"
-sudo mv rhcos-live-rootfs.x86_64.img /var/www/html/rhcos/rootfs.img
-sudo restorecon -RFv /var/www/html/rhcos
-echo -e "\nApache Setup Complete\n"
-echo -e "OK" >> $LOGFILE
+    echo -e "Installing Apache ..\n"
+    echo -e "Installing Apache ..\n" >>$LOGFILE
+    sudo yum -y remove httpd >>$LOGFILE
+    sudo yum -y install httpd >>$LOGFILE
+    on_error $? "Could not install Apache. Check logs at $LOGFILE"
+    sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
+    systemctl enable --now httpd
+    sudo firewall-cmd --add-port=8080/tcp --permanent
+    sudo firewall-cmd --reload
+    sudo mkdir -p /var/www/html/rhcos
+    wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/rhcos-live-rootfs.x86_64.img >>$LOGFILE
+    on_error $? "Could not download Red Hat CoreOSrootfs image. Check logs at $LOGFILE"
+    sudo mv rhcos-live-rootfs.x86_64.img /var/www/html/rhcos/rootfs.img
+    sudo restorecon -RFv /var/www/html/rhcos
+    echo -e "\nApache Setup Complete\n"
+    echo -e "OK" >>$LOGFILE
 
-echo -e "\nConfiguring TFTP Server ..\n"
-echo -e "\nConfiguring TFTP Server ..\n" >> $LOGFILE
-ansible-playbook tasks/configure_tftp_pxe.yml >> $LOGFILE
-on_error $? "Issue Setting up TFTP Server. Check logs at $LOGFILE"
-echo -e "\nTFTP Setup Complete\n"
+    echo -e "\nConfiguring TFTP Server ..\n"
+    echo -e "\nConfiguring TFTP Server ..\n" >>$LOGFILE
+    ansible-playbook tasks/configure_tftp_pxe.yml >>$LOGFILE
+    on_error $? "Issue Setting up TFTP Server. Check logs at $LOGFILE"
+    echo -e "\nTFTP Setup Complete\n"
 
-echo -e "\nInstalling HAProxy ..\n"
-echo -e "\nInstalling HAProxy ..\n" >> $LOGFILE
-sudo yum remove -y haproxy >> $LOGFILE
-sudo yum install -y haproxy >> $LOGFILE
-on_error $? "Issue Installing HAProxy. Check logs at $LOGFILE"
-sudo setsebool -P haproxy_connect_any 1
-sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.default
+    echo -e "\nInstalling HAProxy ..\n"
+    echo -e "\nInstalling HAProxy ..\n" >>$LOGFILE
+    sudo yum remove -y haproxy >>$LOGFILE
+    sudo yum install -y haproxy >>$LOGFILE
+    on_error $? "Issue Installing HAProxy. Check logs at $LOGFILE"
+    sudo setsebool -P haproxy_connect_any 1
+    sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.default
 
-echo -e "\nConfiguring HAProxy ..\n"
-echo -e "\nConfiguring HAProxy ..\n" >> $LOGFILE
-ansible-playbook tasks/configure_haproxy_lb.yml >> $LOGFILE
-on_error $? "Issue Configuring HAProxy. Check logs at $LOGFILE"
-sudo semanage port  -a 6443 -t http_port_t -p tcp  >> $LOGFILE
-sudo semanage port  -a 22623 -t http_port_t -p tcp >> $LOGFILE
-sudo semanage port -a 32700 -t http_port_t -p tcp >> $LOGFILE
-on_error $? "Issue Configuring HAProxy. Check logs at $LOGFILE"
-sudo firewall-cmd --add-service={http,https} --permanent
-sudo firewall-cmd --add-port={6443,22623}/tcp --permanent
-sudo firewall-cmd --reload
-echo -e "\nHAProxy Setup Complete\n"
+    echo -e "\nConfiguring HAProxy ..\n"
+    echo -e "\nConfiguring HAProxy ..\n" >>$LOGFILE
+    ansible-playbook tasks/configure_haproxy_lb.yml >>$LOGFILE
+    on_error $? "Issue Configuring HAProxy. Check logs at $LOGFILE"
+    sudo semanage port -a 6443 -t http_port_t -p tcp >>$LOGFILE
+    sudo semanage port -a 22623 -t http_port_t -p tcp >>$LOGFILE
+    sudo semanage port -a 32700 -t http_port_t -p tcp >>$LOGFILE
+    on_error $? "Issue Configuring HAProxy. Check logs at $LOGFILE"
+    sudo firewall-cmd --add-service={http,https} --permanent
+    sudo firewall-cmd --add-port={6443,22623}/tcp --permanent
+    sudo firewall-cmd --reload
+    echo -e "\nHAProxy Setup Complete\n"
+    echo -e "OK" >>$LOGFILE
 
-systemctl status dhcpd
-systemctl status haproxy
-systemctl status tftp
-
+    systemctl status dhcpd
+    systemctl status haproxy
+    systemctl status tftp
 else
     echo "Cannot find config file. QUITING"
 fi
