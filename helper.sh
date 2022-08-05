21a6c1d7444013336db72a9d197a812d6f93e814
Chris K.T <criskahiga@gmail.com>
2022-08-05 10:58:59 +0300

    Bug fix

--- helper.sh
+++ helper.sh
@@ -50,19 +50,5 @@ set_progress() {
     sed -i "s/$1=0/$1=OK/g" $WORK_DIR/set_progress.sh
 }
 reset_progress() {
-    echo 'CONFIGS=0
-DHCP=0
-TFTP_INSTALL=0
-TFTP_SETUP=0
-HAPROXY=0
-PRE_REQS=0
-ALL_RUNNING=0
-APACHE=0
-OPENSHIFT_CLIENTS=0
-RHCOS_KERNEL=0
-RHCOS_INSTALLER=0
-RHCOS_ROOTFS=0
-OCP_INSTALLER=0
-OCP_FILES=0
-RESUME=0' > $WORK_DIR/set_progress.sh
+    sed -i "s/OK/0/g" $WORK_DIR/set_progress.sh
 }
